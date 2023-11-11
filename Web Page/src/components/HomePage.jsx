a029b592323053a027e7322533a8631933cf8a90
Guilermo1212 <80720696+Guilermo1212@users.noreply.github.com>
2023-11-10 21:42:13 -0500

    Add files via upload

---
+++ Web Page/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PopularPlaces from './PopularPlaces'
+import CommunitiesMini from './CommunitiesMini';
+
+export const Home = () => {
+  return (
+    <div>
+      <PopularPlaces/>
+      <CommunitiesMini/>
+    </div>
+  )
+}
+export default Home;
